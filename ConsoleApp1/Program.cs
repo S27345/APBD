1691c1b204c3bc9c54986ae991ec8b197c18b865
unknown <s27345@pjwstk.edu.pl>
2025-03-09 14:49:49 +0100

    Modyfikacja 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld;
+
+internal class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Hello, World!");
+        var a = 2;
+    }
+}

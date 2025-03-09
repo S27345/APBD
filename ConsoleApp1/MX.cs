7de2f8191fb43a2aa44469b91e0c5bb2aebc89f2
unknown <s27345@pjwstk.edu.pl>
2025-03-09 18:59:34 +0100

    Added MX.cs

---
+++ ConsoleApp1/MX.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MX
+    {
+        public int getMax(int[] ints)
+        {
+            int max = int.MinValue;
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] > max)
+                {
+                    max = ints[i];
+                }
+            }
+            return max;
+        }
+    }
+}

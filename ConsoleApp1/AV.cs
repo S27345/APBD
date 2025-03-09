b367014a14190610f9c2c98aba43feb17368ad0e
unknown <s27345@pjwstk.edu.pl>
2025-03-09 18:39:08 +0100

    Added AV.cs

---
+++ ConsoleApp1/AV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AV
+    {
+        public float getAverage(int[] ints)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                sum += ints[i];
+            }
+            return sum / ints.Length;
+        }
+    }
+}
